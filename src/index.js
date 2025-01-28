9c8746f8d2d6c4ab3fd7423603d50a01256b44c3
Eastyn <eastynwork@gmail.com>
2025-01-28 22:08:33 +0900

    feat($useState) - create useTabs to learn how to create a hook with useState (#1.3 useTabs)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import useInput from './useInput';
-import './index.css';
 import './App.css';
 
-// update an input
+const content = [
+	{
+		tab: 'Section 1',
+		content: 'I am the content of the Section 1',
+	},
+	{
+		tab: 'Section 2',
+		content: 'I am the content of the Section 2',
+	},
+];
+
+const useTabs = (initialTab, allTabs) => {
+	const [currentIndex, setCurrentIndex] = useState(initialTab);
+	if (!allTabs || !Array.isArray(allTabs)) {
+		return;
+	}
+	return {
+		currentItem: allTabs[currentIndex],
+		changeItem: setCurrentIndex,
+	};
+};
 
 const App = () => {
-	// const maxLength = value => value.length <= 10;
-	const noEmail = value => !value.includes('@');
-	const name = useInput('Ms.', noEmail);
+	const { currentItem, changeItem } = useTabs(0, content);
 	return (
 		<div className="App">
 			<h1>Hello</h1>
-			<input placeholder="Name" {...name} />
+			{content.map((section, index) => (
+				<button onClick={() => changeItem(index)}>{section.tab}</button>
+			))}
+			<div>{currentItem.content}</div>
 		</div>
 	);
 };
