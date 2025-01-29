785822a282f844949c05261c36a270e12c3926b4
Eastyn <eastynwork@gmail.com>
2025-01-29 23:09:27 +0900

    feat($useEffect) - on the process of creating useClick but I am lost help!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 
 // what is reference?
 // same with doing document.getElementById()
+const useClick = onClick => {
+	// 변수를 안에서 copy해야 함(ref가 unmount 시점에 null 이 됨)
+	const ref = useRef();
+	useEffect(() => {
+		const element = ref.current;
+		// useEffect works on ComponentDidMount
+		if (typeof onClick !== 'function') {
+			return;
+		}
+		if (element) {
+			element.addEventListener('click', onClick);
+		}
+		// we need to clean this event when ComponentWillUnMount
+		return () => {
+			if (element) {
+				element.removeEventListener('click', onClick);
+			}
+		};
+	}, []);
+	return typeof onClick !== 'function' ? undefined : ref.current; // return the same reference
+};
 const App = () => {
-	const input = useRef();
-	setTimeout(() => input.current.focus(), 5000);
+	const sayHello = () => console.log('say Hello');
+	const title = useClick(sayHello);
 	return (
 		<div className="App">
-			<h1>Hi</h1>
-			<input ref={input} placeholder="la" />
+			<h1 ref={title}>Hi</h1>
 		</div>
 	);
 };
